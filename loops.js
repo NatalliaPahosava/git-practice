5b0d82a7fd258fca95117447dc89151ab2c2e23a
NatalliaPahosava <mulato4ka114@gmail.com>
2022-05-30 20:41:55 -0400

    covered loops and moregit

---
+++ loops.js
@@ -0,0 +1,18 @@
+// if(){}
+// switch(){}
+// for(let i = 0; i > 10; i++  ){
+//     console.log('loop is running',i)
+// }
+
+// for (let i =0; i<=100; i++){
+// console.log('just testing',i)
+// }
+const cars = ['Chevy','Volvo','Saab','Ford','Ferrari','Tesla','Nissan']
+for(let i= 0; i< cars.length; i++ ){ 
+    if(i >= 4 && i<6){
+        console.log('here are high end cars',cars[i])
+    }
+
+
+    // console.log('cars',cars[i])
+}
